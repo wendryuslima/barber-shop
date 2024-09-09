cb88e347ce2643c7420f5eeb7d27f418400bacde
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-09-09 16:15:55 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -28,6 +31,30 @@ const Home = () => {
               className="rounded-xl object-cover"
             />
           </div>
+
+          <Card className="mt-6">
+            <CardContent className="flex justify-between p-0">
+              {/* {Esquerda} */}
+              <div className="flex flex-col gap-2 py-5 pl-5">
+                <Badge>Confirmado</Badge>
+                <h3>Corte de cabelo</h3>
+
+                <div className="flex items-center gap-2 py-3">
+                  <Avatar className="h-6 w-6">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                  </Avatar>
+                  <p className="text-sm">Vintage Barber</p>
+                </div>
+              </div>
+
+              {/* {Direita} */}
+              <div className="flex flex-col items-center justify-center gap-3 border-l-2 border-solid px-5 pr-5">
+                <p className="text-sm">Setembro</p>
+                <p className="text-xl">06</p>
+                <p className="text-sm">09:45</p>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </>
     </div>
