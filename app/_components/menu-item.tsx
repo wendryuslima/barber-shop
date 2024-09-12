9fb143146652c80f10b0341de4a3561b7a046857
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-09-11 21:18:27 -0300

    chore: add next auth setup

--- app/_components/menu-item.tsx
+++ app/_components/menu-item.tsx
@@ -1,10 +1,18 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
-import { Avatar, AvatarImage } from "./ui/avatar"
+// import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import quickSearchOptions from "../_constants/search"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const MenuItem = () => {
   return (
@@ -13,15 +21,36 @@ const MenuItem = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-2 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-2 border-b border-solid py-5">
+        <h2 className="flex font-bold">Olá, faça seu loguin</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conect-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="flex gap-2 font-bold">
+              <Image src="/google.svg" width={18} height={18} alt="google" />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Avatar>
 
         <div className="flex flex-col">
           <p className="font-bold">Wendryus Lima</p>
           <p className="text-xs text-gray-400">wendryuslima31@gmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-4 border-b border-solid py-5">
