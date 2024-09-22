ec22a7946c8ce9e881c459608ddc01cae833c762
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-09-22 17:40:55 -0300

    route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,7 @@
 import { authOptions } from "@/app/_lib/auth"
-import NextAuth from "next-auth"
-import { NextApiRequest, NextApiResponse } from "next"
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    await NextAuth(req, res, authOptions)
-  } catch (error) {
-    console.error("Erro na autenticação:", error)
-    res.status(500).json({ error: "Erro na autenticação" })
-  }
-}
+import NextAuth from "next-auth/next"
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
