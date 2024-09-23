9ae435c3528bde5da48d2f1617e2b29b4be799eb
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-09-23 10:15:30 -0300

    new correction

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -8,4 +8,6 @@ export const db =
     log: ["query"],
   })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db
+}
