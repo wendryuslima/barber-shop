bb8f12e5dc23f0bbd0d847debf80da07f652cd74
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-09-23 10:42:33 -0300

    chore: barber-shop page

--- app/barbershop/page.tsx
+++ app/barbershop/page.tsx
@@ -1,60 +1,62 @@
-import BarberShopItem from "../_components/barbershop-item"
+import BarbershopItem from "../_components/barbershop-item"
 import Header from "../_components/header"
 import Search from "../_components/search"
 import { db } from "../_lib/prisma"
 
-interface BarberShopPageProps {
+interface BarbershopsPageProps {
   searchParams: {
-    search?: string
+    title?: string
+    service?: string
   }
 }
 
-const BarberShopPage = async ({ searchParams }: BarberShopPageProps) => {
-  try {
-    const searchQuery = searchParams?.search || ""
-
-    const barbershops = await db.barbershop.findMany({
-      where: {
-        OR: [
-          {
-            name: {
-              contains: searchQuery,
-              mode: "insensitive",
-            },
-          },
-          {
-            services: {
-              some: {
-                name: {
-                  contains: searchQuery,
-                  mode: "insensitive",
+const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
+  const barbershops = await db.barbershop.findMany({
+    where: {
+      OR: [
+        searchParams?.title
+          ? {
+              name: {
+                contains: searchParams?.title,
+                mode: "insensitive",
+              },
+            }
+          : {},
+        searchParams.service
+          ? {
+              services: {
+                some: {
+                  name: {
+                    contains: searchParams.service,
+                    mode: "insensitive",
+                  },
                 },
               },
-            },
-          },
-        ],
-      },
-    })
-    return (
-      <div>
-        <Header />
-        <div className="my-6 px-5">
-          <Search />
-        </div>
-        <div className="px-5">
-          <h2>Resultados para {searchParams.search}</h2>
-          <div className="grid grid-cols-2 gap-4 md:flex md:overflow-x-auto md:[&::-webkit-scrollbar]:hidden">
-            {barbershops.map((barbershop) => (
-              <BarberShopItem barbershop={barbershop} key={barbershop.id} />
-            ))}
-          </div>
+            }
+          : {},
+      ],
+    },
+  })
+
+  return (
+    <div>
+      <Header />
+      <div className="my-6 px-5">
+        <Search />
+      </div>
+      <div className="px-5">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Resultados para &quot;{searchParams?.title || searchParams?.service}
+          &quot;
+        </h2>
+        <div className="grid grid-cols-2 gap-4">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
         </div>
       </div>
-    )
-  } catch (error) {
-    console.error("Erro ao buscar barbearias:", error)
-    return <div>Error loading barbershops</div>
-  }
+    </div>
+  )
 }
 
-export default BarberShopPage
+export default BarbershopsPage
